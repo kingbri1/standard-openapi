0af88ad5741a69ee19f03e9b366be8a2d2c0bb70
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-05-28 22:34:04 -0400

    Package: Add debug tests
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- packages/core/src/zod.ts
+++ packages/core/src/zod.ts
@@ -4,7 +4,9 @@ import convert from "./convertor.js";
 
 // Fallback to use schemaType as input if metadata isn't provided
 export const generator: GeneratorFn = async (schema) => {
+  console.log(schema);
   const jsonSchema = z.toJSONSchema(schema as z.$ZodType, { io: "input", target: "draft-7" });
+  console.log(jsonSchema)
 
   return {
     schema: await convert(jsonSchema),
